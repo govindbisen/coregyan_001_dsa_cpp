329ba2327573c5f4e06161c6da703252cc650002
unknown <govind.bisen@gmail.com>
2026-08-24 23:07:33 +0530

    ok 14

---
+++ 14_structureAsParameterCBR.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct rectangle
+{
+    int length;
+    int breadth;
+};
+
+void fun(struct rectangle &r)
+{
+    cout << (r).length << endl;
+    cout << (r).breadth << endl;
+};
+
+int main()
+{
+    struct rectangle r = {10, 20};
+    printf("main : Breadth %d  Length %d\n", r.breadth, r.length);
+    printf("fun:\n");
+    fun(r);
+
+    return 0;
+}
